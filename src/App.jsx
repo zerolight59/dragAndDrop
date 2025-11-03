e7480c2c7d07aed01ad100f373dff0f8d56144df
zerolight59 <manusankaru10@gmail.com>
2025-11-04 03:41:42 +0530

    added modifier restrictToWindowEdges

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {closestCorners, DndContext} from '@dnd-kit/core';
 import './App.css'
 import { Column } from './Components/Column/Column';
 import { arrayMove } from '@dnd-kit/sortable';
+import { restrictToFirstScrollableAncestor, restrictToParentElement, restrictToWindowEdges } from '@dnd-kit/modifiers';
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -30,7 +31,7 @@ function App() {
     <div className='app'>
       <h1>My task</h1>
       <div className='dragarea'>
-        <DndContext collisionDetection={closestCorners} onDragEnd={handleDragEnd}>
+        <DndContext collisionDetection={closestCorners} onDragEnd={handleDragEnd} modifiers={[restrictToWindowEdges]}>
           <Column tasks={tasks}/>
         </DndContext>
       </div>  
--- src/Components/Column/Column.jsx
+++ src/Components/Column/Column.jsx
@@ -14,6 +14,7 @@ export const Column = ({tasks}) => {
         }
         </SortableContext>
         
+        
     </div>
   )
 }
